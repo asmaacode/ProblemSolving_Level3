1178735e702a6398f827b158277a43eca11b6a07
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-09-01 09:37:49 +0300

    Update #07 TransposeMatrix.cpp

--- #07 TransposeMatrix.cpp
+++ #07 TransposeMatrix.cpp
@@ -1,27 +1,38 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
+const int rows = 3, cols = 3;
 
-void fill2DArray(int arr[3][3]) {
+void fillOrdered2DArray(int arr[rows][cols], int rows, int cols) {
 	short counter = 1;
-	for (short row = 0; row < 3; row++)
-		for (short col = 0; col < 3; col++) {
+	for (short row = 0; row < rows; row++)
+		for (short col = 0; col < cols; col++) {
 			arr[row][col] = counter;
 			counter++;
 		}
 
 }
-void printTranspose2DArray(int arr[3][3]) {
-	for (short row = 0; row < 3; row++) {
-		for (short col = 0; col < 3; col++) {
+void transposeMatrix(int arr[rows][cols], int rows, int cols) {
+	short counter = 1;
+	for (short row = 0; row < rows; row++) {
+		for (short col = 0; col < cols; col++) {
+			arr[col][row] = counter;
+			counter++;
+		}
+	}
+
+}
+void printTranspose2DArray(int arr[rows][cols]) {
+	for (short row = 0; row < rows; row++) {
+		for (short col = 0; col < rows; col++) {
 			cout << setw(3) << arr[col][row] << "   ";
 		}
 		cout << endl;
 	}
 }
-void print2DArrayElements(int arr[3][3]) {
-	for (short row = 0; row < 3; row++) {
-		for (short col = 0; col < 3; col++) {
+void print2DArrayElements(int arr[rows][cols]) {
+	for (short row = 0; row < rows; row++) {
+		for (short col = 0; col < rows; col++) {
 			cout << setw(3) << arr[row][col] << "   ";
 		}
 		cout << endl;
@@ -29,12 +40,15 @@ void print2DArrayElements(int arr[3][3]) {
 }
 
 int main() {
-	int arr[3][3];
-	fill2DArray(arr);
-	cout << "The following is a 3 * 3 ordered matrix: \n";
+	int arr[rows][cols], transposedArr[rows][cols];
+	fillOrdered2DArray(arr,rows,cols);
+	cout << "The following is a " << rows << " * " << cols << " ordered matrix: \n";
 	print2DArrayElements(arr);
-	cout << "\n\nThe following is a 3 * 3 transposed matrix: \n";
-	printTranspose2DArray(arr);
+
+	transposeMatrix(transposedArr,rows,cols);
+
+	cout << "\n\nThe following is a " << rows << " * " << cols << " transposed matrix: \n";
+	print2DArrayElements(transposedArr);
 	return 0;
 	//system("pause > 0");
 }
