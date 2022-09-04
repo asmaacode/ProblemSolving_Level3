cc9cebf640429cd7b3e1a504ed2b6c939bac07ab
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-09-04 21:41:55 +0300

    Update #16 SparseMatrix.cpp

--- #16 SparseMatrix.cpp
+++ #16 SparseMatrix.cpp
@@ -13,19 +13,17 @@ void fill2DArrayRandomly(int arr[rows][cols], int rows, int cols) {
 		}
 
 }
-
-
 void print2DArrayElements(int arr[rows][cols], short rows, short cols) {
 	for (short row = 0; row < rows; row++) {
 		for (short col = 0; col < rows; col++) {
-			printf("%0*d",2, arr[row][col]);
+			printf("%0*d", 2, arr[row][col]);
 			cout << setw(4) << "   ";
 		}
 		cout << endl;
 	}
 }
 
-int countNumberInMatrix(int arr[rows][cols], short rows, short cols,int target) {
+int countNumberInMatrix(int arr[rows][cols], short rows, short cols, int target) {
 	int count = 0;
 	for (short row = 0; row < rows; row++) {
 		for (short col = 0; col < rows; col++) {
@@ -37,12 +35,18 @@ int countNumberInMatrix(int arr[rows][cols], short rows, short cols,int target) 
 int enterTheTargetNumber() {
 	return(readNumberMsg("Enter the number to count it :"));
 }
+bool IsSparseMatrix(int matrixSize, int zerosCounts) {
+	return zerosCounts > matrixSize - zerosCounts;
+}
 int main() {
 	srand(time(NULL));
-	int arr[rows][cols]={{1,0,0},{5,3,0},{0,0,0}};
+	int arr[rows][cols] = { {1,0,0},{5,3,0},{0,0,0} };
 	//fill2DArrayRandomly(arr,rows,cols);
-	print2DArrayElements(arr,rows,cols);
+	print2DArrayElements(arr, rows, cols);
 	int zerosCounts = countNumberInMatrix(arr, rows, cols, 0);
-	int otherElementsCount = (rows * cols) - zerosCounts;
-	if (zerosCounts > otherElementsCount) cout << "The matrix is a sparse\n";else cout << "The matrix is not a sparse\n";
+	int matrixSize = rows * cols;
+	if (IsSparseMatrix(matrixSize, zerosCounts))
+		cout << "The matrix is a sparse\n";
+	else
+		cout << "The matrix is not a sparse\n";
 }
