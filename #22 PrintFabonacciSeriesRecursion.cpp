3ae28ff1d6de1448c42c03bf880ae71f9eaaca92
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-09-10 13:21:54 +0300

    Update #22 PrintFabonacciSeriesRecursion.cpp

--- #22 PrintFabonacciSeriesRecursion.cpp
+++ #22 PrintFabonacciSeriesRecursion.cpp
@@ -4,24 +4,6 @@
 using namespace std;
 using namespace myLibrary;
 
-const short rows = 3, cols = 3;
-
-void fill2DArrayRandomly(int arr[rows][cols], int rows, int cols) {
-	for (short row = 0; row < rows; row++)
-		for (short col = 0; col < cols; col++) {
-			arr[row][col] = generateRandomNumbers(0, 10);
-		}
-
-}
-void print2DArrayElements(int arr[rows][cols], short rows, short cols) {
-	for (short row = 0; row < rows; row++) {
-		for (short col = 0; col < cols; col++) {
-			printf("%0*d", 2, arr[row][col]);
-			cout << setw(4) << "   ";
-		}
-		cout << endl;
-	}
-}
 void printFabonacciSeries(int num,int prev,int afterPrev) {
 	int answer = 0;
 	if (num > 0) {
