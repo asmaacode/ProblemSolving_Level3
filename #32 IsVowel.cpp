ac5d623d72b640810c195b5c507334b8f3e99ffb
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-09-12 20:14:35 +0300

    Update #32 IsVowel.cpp

--- #32 IsVowel.cpp
+++ #32 IsVowel.cpp
@@ -6,11 +6,16 @@ using namespace myLibrary;
 
 bool isVowel(char& letter) {
 	string vowels = "aeoui";
-	return (vowels.find(tolower(letter)) != vowels.npos) ? true : false;
+	letter = tolower(letter);
+	return (vowels.find(letter) != vowels.npos) ? true : false;
+}
+bool isVowel_pure(char& letter) {
+	letter = tolower(letter);
+	return (letter =='a') || (letter =='e') || (letter =='i') || (letter == 'o') ||(letter == 'u');
 }
 int main() {
 	char letter = readChar("Please enter the letter to check it :");
-	if (isVowel(letter)) {
+	if (isVowel(letter)) {//if (isVowel_pure(letter)) {
 		cout << "\nYes,the Letter '" << letter << "' is vowel\n";
 	}
 	else {
